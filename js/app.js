d759e83bf2782aabb6bf6fadc17089fb6e58b441
Hardik Avaiya <v-harav@microsoft.com>
2017-08-30 23:15:37 +0530

    added map

--- js/app.js
+++ js/app.js
@@ -6,19 +6,19 @@ app.config(["$routeProvider", "$locationProvider", function ($routeProvider, $lo
         controller: "HomeController"
     }).when("/hiretalent", {
         templateUrl: "/placeholder.html",
-        controller: "HomeController"
+        controller: "HireTalentController"
     }).when("/expertise", {
         templateUrl: "/placeholder.html",
-        controller: "HomeController"
+        controller: "ExpertiseController"
     }).when("/findwork", {
         templateUrl: "/placeholder.html",
-        controller: "HomeController"
+        controller: "FindWorkController"
     }).when("/benefits", {
         templateUrl: "/placeholder.html",
-        controller: "HomeController"
+        controller: "BenefitsController"
     }).when("/contacts", {
         templateUrl: "/placeholder.html",
-        controller: "HomeController"
+        controller: "ContactController"
     })
         .otherwise({ redirectTo: "/" });
     $locationProvider.html5Mode(true);
@@ -29,6 +29,85 @@ app.config(["$routeProvider", "$locationProvider", function ($routeProvider, $lo
         navigate();
     });
     $scope.$on('$routeChangeSuccess', function () {
+
+        console.log('Route Change: ' + $location.url());
+        $window.ga('set', 'page', $location.url());
+        $window.ga('send', 'pageview', {
+            'hitCallback': function () {
+                console.log('GA hitCallback sent!');
+            }
+        });
+    });
+}]).controller('HireTalentController', ["$scope", "$location", "$window", function ($scope, $location, $window) {
+    $scope.$on('$viewContentLoaded', function () {
+        loadPlugins();
+        navigate();
+    });
+    $scope.$on('$routeChangeSuccess', function () {
+
+        console.log('Route Change: ' + $location.url());
+        $window.ga('set', 'page', $location.url());
+        $window.ga('send', 'pageview', {
+            'hitCallback': function () {
+                console.log('GA hitCallback sent!');
+            }
+        });
+    });
+}]).controller('ExpertiseController', ["$scope", "$location", "$window", function ($scope, $location, $window) {
+    $scope.$on('$viewContentLoaded', function () {
+        loadPlugins();
+        navigate();
+    });
+    $scope.$on('$routeChangeSuccess', function () {
+
+        console.log('Route Change: ' + $location.url());
+        $window.ga('set', 'page', $location.url());
+        $window.ga('send', 'pageview', {
+            'hitCallback': function () {
+                console.log('GA hitCallback sent!');
+            }
+        });
+    });
+}]).controller('FindWorkController', ["$scope", "$location", "$window", function ($scope, $location, $window) {
+    $scope.$on('$viewContentLoaded', function () {
+        loadPlugins();
+        navigate();
+    });
+    $scope.$on('$routeChangeSuccess', function () {
+
+        console.log('Route Change: ' + $location.url());
+        $window.ga('set', 'page', $location.url());
+        $window.ga('send', 'pageview', {
+            'hitCallback': function () {
+                console.log('GA hitCallback sent!');
+            }
+        });
+    });
+}]).controller('BenefitsController', ["$scope", "$location", "$window", function ($scope, $location, $window) {
+    $scope.$on('$viewContentLoaded', function () {
+        loadPlugins();
+        navigate();
+    });
+    $scope.$on('$routeChangeSuccess', function () {
+
+        console.log('Route Change: ' + $location.url());
+        $window.ga('set', 'page', $location.url());
+        $window.ga('send', 'pageview', {
+            'hitCallback': function () {
+                console.log('GA hitCallback sent!');
+            }
+        });
+    });
+}]).controller('ContactController', ["$scope", "$location", "$window", function ($scope, $location, $window) {
+    $scope.$on('$viewContentLoaded', function () {
+        loadPlugins();
+        navigate();
+    });
+    $scope.$on('$routeChangeSuccess', function () {
+        //var googlemaps = document.createElement('script');
+        //googlemaps.setAttribute('src', 'https://maps.googleapis.com/maps/api/js?v=3&sensor=true&key=AIzaSyBHkJlVTrAkgcD9jJxo_CyHx0YZZtX65iY&callback=initMap');
+        //document.head.appendChild(googlemaps);
+
         console.log('Route Change: ' + $location.url());
         $window.ga('set', 'page', $location.url());
         $window.ga('send', 'pageview', {
