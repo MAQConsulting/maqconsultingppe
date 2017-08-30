f72020f5c04132834a9c771a35001d75bff1bebe
Hardik Avaiya <v-harav@microsoft.com>
2017-08-30 21:17:26 +0530

    removed unwanted comments

--- js/app.js
+++ js/app.js
@@ -2,27 +2,21 @@
 app.config(["$routeProvider", "$locationProvider", function ($routeProvider, $locationProvider) {
     $routeProvider
     .when("/", {
-        //templateUrl: "/views/home.html",
         templateUrl: "/placeholder.html",
         controller: "HomeController"
     }).when("/hiretalent", {
-        //templateUrl: "/views/home.html",
         templateUrl: "/placeholder.html",
         controller: "HomeController"
     }).when("/expertise", {
-        //templateUrl: "/views/home.html",
         templateUrl: "/placeholder.html",
         controller: "HomeController"
     }).when("/findwork", {
-        //templateUrl: "/views/home.html",
         templateUrl: "/placeholder.html",
         controller: "HomeController"
     }).when("/benefits", {
-        //templateUrl: "/views/home.html",
         templateUrl: "/placeholder.html",
         controller: "HomeController"
     }).when("/contacts", {
-        //templateUrl: "/views/home.html",
         templateUrl: "/placeholder.html",
         controller: "HomeController"
     })
